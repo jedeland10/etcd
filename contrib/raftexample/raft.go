f74da5619c0470c35669bbad3752ee929820673c
jedeland10 <johan.edeland@live.com>
2025-05-19 16:19:54 +0200

    bump gc percentage

--- contrib/raftexample/raft.go
+++ contrib/raftexample/raft.go
@@ -32,7 +32,6 @@ import (
 	"go.etcd.io/etcd/server/v3/storage/wal/walpb"
 	"go.etcd.io/raft/v3"
 	"go.etcd.io/raft/v3/raftpb"
-	"golang.org/x/time/rate"
 
 	"go.uber.org/zap"
 )
@@ -269,15 +268,13 @@ func (rc *raftNode) startRaft() {
 	rc.node = raft.StartNode(c, rpeers)
 
 	rc.transport = &rafthttp.Transport{
-		Logger:             rc.logger,
-		ID:                 types.ID(rc.id),
-		ClusterID:          0x1000,
-		DialTimeout:        10 * time.Second,
-		DialRetryFrequency: rate.Limit(10),
-		Raft:               rc,
-		ServerStats:        stats.NewServerStats("", ""),
-		LeaderStats:        stats.NewLeaderStats(zap.NewExample(), strconv.Itoa(rc.id)),
-		ErrorC:             make(chan error),
+		Logger:      rc.logger,
+		ID:          types.ID(rc.id),
+		ClusterID:   0x1000,
+		Raft:        rc,
+		ServerStats: stats.NewServerStats("", ""),
+		LeaderStats: stats.NewLeaderStats(zap.NewExample(), strconv.Itoa(rc.id)),
+		ErrorC:      make(chan error),
 	}
 
 	rc.transport.Start()
