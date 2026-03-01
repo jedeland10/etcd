08447c281804c1523fe6d4b4f2382c2932407d9a
jedeland10 <johan.edeland@live.com>
2026-03-01 23:46:19 +0100

    election tick 10 ms

--- contrib/raftexample/raft.go
+++ contrib/raftexample/raft.go
@@ -257,7 +257,7 @@ func (rc *raftNode) startRaft() {
 	}
 	c := &raft.Config{
 		ID:                        uint64(rc.id),
-		ElectionTick:              100,
+		ElectionTick:              10,
 		HeartbeatTick:             1,
 		Storage:                   rc.raftStorage,
 		MaxSizePerMsg:             1024 * 1024,
