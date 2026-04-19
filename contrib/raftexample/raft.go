3e511c2576ec8adb74af9bd456f890b8f5a755f0
jedeland10 <johan.edeland@live.com>
2026-04-19 20:37:47 +0200

    up cache size

--- contrib/raftexample/raft.go
+++ contrib/raftexample/raft.go
@@ -280,7 +280,7 @@ func (rc *raftNode) startRaft() {
 		MaxSizePerMsg:             1024 * 1024,
 		MaxInflightMsgs:           1_000_000,
 		MaxUncommittedEntriesSize: 1 << 30,
-		UniCacheSize:              75_000,
+		UniCacheSize:              25_000,
 	}
 
 	rc.node = raft.StartNode(c, rpeers)
